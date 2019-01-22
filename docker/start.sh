83fe5e65b5b6795da52d5a16fb53499945145444
marcin <marcin.kasinski@gmail.com>
2019-01-22 13:55:09 +0100

    running version

---
+++ docker/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar -Dspring.profiles.active=$SPRING_PROFILE SpringBootSoapService-0.0.1-SNAPSHOT.jar
--- src/main/java/hello/CountryEndpoint.java
+++ src/main/java/hello/CountryEndpoint.java
@@ -1,10 +1,17 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import io.spring.guides.gs_producing_web_service.GetCountryRequest;
 import io.spring.guides.gs_producing_web_service.GetCountryResponse;
@@ -12,6 +19,7 @@ import io.spring.guides.gs_producing_web_service.GetCountryResponse;
 @Endpoint
 public class CountryEndpoint {
 	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+	private static Logger log = LoggerFactory.getLogger(CountryEndpoint.class);
 
 	private CountryRepository countryRepository;
 
@@ -23,9 +31,32 @@ public class CountryEndpoint {
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
 	@ResponsePayload
 	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+
 		GetCountryResponse response = new GetCountryResponse();
 		response.setCountry(countryRepository.findCountry(request.getName()));
 
+		///////////////////////////////////////////////////////////////////////////////
+		log.info("GetCountryResponse executed");
+		ObjectMapper objectMapper = new ObjectMapper();
+		XmlMapper xmlMapper = new XmlMapper();
+		//pretty xml
+		xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		String json;
+		String xmlrequest;
+		String xmlresponse;
+		try {
+			json = objectMapper.writeValueAsString(request);
+			xmlrequest = xmlMapper.writeValueAsString(request);
+			xmlresponse = xmlMapper.writeValueAsString(response);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			xmlrequest = "UNKNOWN";
+			xmlresponse = "UNKNOWN";
+		}
+		log.info("in:"+xmlrequest);
+		log.info("out:"+xmlresponse);
+		///////////////////////////////////////////////////////////////////////////////
 		return response;
 	}
 }
