71b16a40e50e1fb02cbdcc7ae610fff1593af658
marcin <marcin.kasinski@gmail.com>
2019-01-22 15:26:27 +0100

    add version

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		
+
+		System.out.println("Application ver 22.01.2019");
+
+		
+	System.out.println("ENV variables");
+		
+
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+			System.out.format("%s=%s%n", envName, env.get(envName));
+		}
+
+
+		
 		SpringApplication.run(Application.class, args);
 	}
 }
